de04e5aab6fca63c01c75d94fa1afe2494a2a802
sajjadramezani <sajjad.payam77@gmail.com>
2022-12-07 22:54:26 +0330

    feat: Create function create comment
    fix bug comment model

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,4 +1,5 @@
 const Comment = require("../model/comment")
+const User = require("../model/user")
 const HttpError = require('../model/http-error')
 
 const getComments = async (req, res, next) => {
@@ -6,14 +7,44 @@ const getComments = async (req, res, next) => {
     try {
         comments = await Comment.find()
     } catch (err) {
-        const error = new HttpError('Getting user faild', 500)
+        const error = new HttpError('Getting comment faild', 500)
         return next(error)
     }
     res.json({ comments: comments.map(user => user.toObject({ gettesr: true })) })
 }
 
-const createComment = (req, res, next) => {
+const createComment = async (req, res, next) => {
+    const { title, description, score, creator } = req.body
 
+    const createComment = new Comment({
+        title: title,
+        description: description,
+        score: score,
+        user: creator
+    })
+
+    let user
+    try {
+        user = await User.findById(creator)
+    } catch (err) {
+        const error = new HttpError('Creating comment faildd', 500)
+        return next(error)
+    }
+    if (!user) {
+        res.status(422).json({ success: 0, message: 'کاربری با این آی دی یافت نشد' })
+        return next()
+    }
+
+    try {
+        await createComment.save()
+        user.comments.push(createComment)
+        await user.save()
+    } catch (err) {
+        const error = new HttpError('Creating comment failddd', 500)
+        return next(error)
+    }
+
+    res.status(201).json({ success: 1, message: 'نظر شما با موفقیت ثبت شد' })
 }
 
 exports.getComments = getComments
--- model/comment.js
+++ model/comment.js
@@ -5,8 +5,7 @@ const commentSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     score: { type: Number, required: true },
-    time: { type: String, required: true },
     user: { type: mongoose.Types.ObjectId, required: true, ref: 'User' }
-})
+}, { timestamps: true })
 
 module.exports = mongoose.model('Comment', commentSchema)
