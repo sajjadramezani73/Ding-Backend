d31805a12a18df9b4ac4c07a31346b379923e171
sajjadramezani <sajjad.payam77@gmail.com>
2023-01-11 20:51:18 +0330

    fix: Add totalTime parameter for entryAndExit modal schema

--- model/entryAndExit.js
+++ model/entryAndExit.js
@@ -5,7 +5,8 @@ const entryAndExitSchema = new Schema({
     user: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
     date: { type: String, required: true },
     lastMode: { type: String, required: true },
-    entryAndExit: { type: Array, "default": [] }
+    entryAndExit: { type: Array, "default": [] },
+    totalTime: { type: String, require: true }
 })
 
 module.exports = mongoose.model('EntryAndExit', entryAndExitSchema)
